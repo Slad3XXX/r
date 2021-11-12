3c7a215d2dfcc6f796322348b60cd6a940e4900c
Slad3XXX <68914180+Slad3XXX@users.noreply.github.com>
2021-11-12 17:51:45 -0600

    Initial commit

---
+++ pokemon/js.js
@@ -0,0 +1,48 @@
+const fetchPoke = async() => {
+    var name = $("#search").val().toLowerCase();
+
+    try {
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon/' + name)
+        const data = await res.json();
+
+        console.log(res);
+        console.log(data);
+        fillCard(data);
+
+
+
+    } catch (error) {
+        console.log(error);
+
+    }
+
+
+}
+
+const fillCard = (data) => {
+    // var template = $("#template").contents();
+    const img = data.sprites.front_default;
+    $("#card-img").attr("src", img);
+    const name = data.name;
+    $("#pokename").text(name);
+    console.log(img)
+
+}
+
+
+$(document).ready(function() {
+
+    fetchPoke();
+    fillCard();
+
+
+
+
+
+
+
+
+
+
+
+})
